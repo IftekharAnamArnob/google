9e609e79ef02b6ca88c1d34df97054d6addee047
LoneWolf <arnob5363@gmail.com>
2025-02-17 00:58:03 +0600

    Comments removed!

--- google/src/components/SearchBar.jsx
+++ google/src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types"; // Import PropTypes for validation
+import PropTypes from "prop-types"; 
 
 const SearchBar = ({ placeholder, iconLeft, iconRight, iconRightTwo, size, style }) => {
   return (
@@ -7,28 +7,28 @@ const SearchBar = ({ placeholder, iconLeft, iconRight, iconRightTwo, size, style
       className={`flex items-center border rounded-full p-2 bg-white ${style}`}
       style={{ width: size || "400px" }}
     >
-      {/* Left Icon (Optional) */}
+     
       {iconLeft && (
         <div className="mr-3">
           <img src={iconLeft} alt="Left Icon" className="w-6 h-6" />
         </div>
       )}
 
-      {/* Search Input */}
+      
       <input
         type="text"
         placeholder={placeholder || "Search..."}
         className="w-full p-2 text-black focus:outline-none rounded-full"
       />
 
-      {/* Right Icon (Optional) */}
+      
       {iconRight && (
         <div className="ml-3">
           <img src={iconRight} alt="Right Icon" className="w-6 h-6" />
         </div>
       )}
 
-      {/* Right Icon Two (Optional) */}
+      
       {iconRightTwo && (
         <div className="ml-3">
           <img src={iconRightTwo} alt="Right Icon Two" className="w-6 h-6" />
@@ -38,14 +38,14 @@ const SearchBar = ({ placeholder, iconLeft, iconRight, iconRightTwo, size, style
   );
 };
 
-// Prop validation using PropTypes
+
 SearchBar.propTypes = {
-  placeholder: PropTypes.string, // Expected type is a string
-  iconLeft: PropTypes.string,    // Expected type is a string (image URL or path)
-  iconRight: PropTypes.string,   // Expected type is a string (image URL or path)
-  iconRightTwo: PropTypes.string, // Expected type is a string (image URL or path)
-  size: PropTypes.string,        // Expected type is a string (for size, e.g., "400px")
-  style: PropTypes.string,       // Expected type is a string (for additional custom styles)
+  placeholder: PropTypes.string, 
+  iconLeft: PropTypes.string,    
+  iconRight: PropTypes.string,   
+  iconRightTwo: PropTypes.string, 
+  size: PropTypes.string,        
+  style: PropTypes.string,       
 };
 
 export default SearchBar;
